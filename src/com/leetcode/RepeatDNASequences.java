df7389db4b685e781f8215de151ab563b7543e47
weilun <weilun@staff.weibo.com>
2015-05-13 19:18:35 +0800

    修改重复DNA检测算法，使用hash值代替字符串，避免内存溢出

--- src/com/leetcode/RepeatDNASequences.java
+++ src/com/leetcode/RepeatDNASequences.java
@@ -9,30 +9,42 @@ public class RepeatDNASequences {
 
     public List<String> findRepeatedDnaSequences(String s) {
 
-        if (s == null || s.length() < 10)
+        if (s == null)
             return null;
 
-        int count = s.length() - 10;
-        Set<String> set = new HashSet<>();
         List<String> ans = new ArrayList<>();
+        int count = s.length() - 10;
+        Set<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < count; i++) {
+
+        for (int i = 0; i <= count; i++) {
             String sub = s.substring(i, i + 10);
-            if (set.contains(sub)) {
+            int hashValue = convertDNAStringToInt(sub);
+
+            if (set.contains(hashValue) && !ans.contains(sub)) {
                 ans.add(sub);
                 set.remove(sub);
             } else {
-                if (!ans.contains(sub))
-                    set.add(sub);
+                set.add(hashValue);
             }
         }
 
         return ans;
     }
 
+    private static final int convertDNAStringToInt(String str) {
+        int res = 0;
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            char tmp = str.charAt(i);
+            res |= (tmp & 0x07) << (i * 3);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         RepeatDNASequences rds = new RepeatDNASequences();
-        List<String> dnas = rds.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+        List<String> dnas = rds.findRepeatedDnaSequences("AAAAAAAAAA");
         for (String dna : dnas) {
             System.out.println(dna);
         }
